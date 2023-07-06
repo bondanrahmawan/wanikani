92ff2b992763f07bfde4f7305c67af5165bb1688
arirarahma <arira.rahma@gmail.com>
2023-07-07 00:29:39 +0700

    fix db

--- utilities/connectMongo.tsx
+++ utilities/connectMongo.tsx
@@ -3,8 +3,8 @@ import mongoose from "mongoose";
 const connectDB = async () => {
 	try {
 		await mongoose.connect(
-			// "mongodb+srv://admin:Administrator@cluster0.1dj9w0e.mongodb.net/wanikani"
-			"mongodb://127.0.0.1:27017/admin"
+			"mongodb+srv://admin:Administrator@cluster0.1dj9w0e.mongodb.net/wanikani"
+			// "mongodb://127.0.0.1:27017/admin"
 		);
 		console.log("MongoDB connected");
 	} catch (err) {
