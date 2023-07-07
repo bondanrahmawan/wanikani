9e817cb75ab7453e2a5eaac2cdc0da2f83de090a
arirarahma <arira.rahma@gmail.com>
2023-07-08 02:14:52 +0700

    test

--- utilities/connectMongo.tsx
+++ utilities/connectMongo.tsx
@@ -3,7 +3,8 @@ import mongoose from "mongoose";
 const connectDB = async () => {
 	try {
 		await mongoose.connect(
-			"mongodb+srv://admin:Administrator@cluster0.1dj9w0e.mongodb.net/wanikani"
+			"mongodb+srv://vercel-admin-user:9CHng4fY2M1VzZVr@cluster0.1dj9w0e.mongodb.net/myFirstDatabase?retryWrites=true&w=majority"
+			// "mongodb+srv://admin:Administrator@cluster0.1dj9w0e.mongodb.net/wanikani"
 			// "mongodb://127.0.0.1:27017/admin"
 		);
 		console.log("MongoDB connected");
